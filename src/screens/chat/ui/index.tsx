2ed69a326d37485ba674377a9490b44eb302f11f
Dmytro Baturin <dbaturin02@gmail.com>
2024-08-27 20:19:22 +0200

    ChatInfo

--- src/screens/chat/ui/index.tsx
+++ src/screens/chat/ui/index.tsx
@@ -2,25 +2,29 @@ import { Textarea } from "@/shared/components/ui/textarea";
 import { Button } from "@/shared/components/ui/button";
 import { PaperPlaneIcon } from "@radix-ui/react-icons";
 import { FromMessage, ToMessage } from "@/entities/message";
+import { ChatInfo } from "@/widgets/chatInfo/ui";
 
 export const Chat = ({ id }: { id: string }) => {
   return (
-    <div className="relative p-4 flex-col justify-between flex h-screen">
-      <div className="h-screen flex flex-col gap-4 relative">
-        <FromMessage />
-        <ToMessage />
+    <>
+      <ChatInfo />
+      <div className="relative p-4 flex-col justify-between flex h-screen">
+        <div className="h-screen flex flex-col gap-4 relative">
+          <FromMessage />
+          <ToMessage />
+        </div>
+        <div className="relative h-[80px] w-full ">
+          <Textarea
+            className="resize-none h-full"
+            placeholder="Enter your message"
+            action={
+              <Button size="icon">
+                <PaperPlaneIcon />
+              </Button>
+            }
+          />
+        </div>
       </div>
-      <div className="relative h-[80px] w-full ">
-        <Textarea
-          className="resize-none h-full"
-          placeholder="Enter your message"
-          action={
-            <Button size="icon">
-              <PaperPlaneIcon />
-            </Button>
-          }
-        />
-      </div>
-    </div>
+    </>
   );
 };
---
+++ src/widgets/chatInfo/ui/index.tsx
@@ -0,0 +1,13 @@
+interface ChatInfoProps {
+  name?: string;
+  lastSeen?: string;
+  members?: string[];
+}
+
+export const ChatInfo = ({ name, members, lastSeen }: ChatInfoProps) => {
+  return (
+    <div className="flex justify-center shadow-sm w-full h-[50px]">
+      ChatInfo
+    </div>
+  );
+};
